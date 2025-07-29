5458f4559ef407d6c3797ddeda43549d6b2f7be7
Akshit Bhardwaj <80941889+AkshitBh08@users.noreply.github.com>
2025-07-29 16:16:17 +0530

    Update pinghost.py

--- core/pinghost.py
+++ core/pinghost.py
@@ -3,11 +3,11 @@ from colorama import Fore, Style
 def ping_host(host):
     try:
         nm = nmap.PortScanner()
-        nm.scan(host, '1-1024')  # Scan a range of ports just to check if the host is up
+        nm.scan(host, '1-1024')
         if nm.all_hosts():
-            return "Online", 0  # Host is up, return "Online" and set latency to 0
+            return "Online", 0
         else:
-            return "Offline", None  # Host is not up
+            return "Offline", None
     except Exception as e:
         print(Fore.RED + f"Error pinging host {host}: {e}" + Style.RESET_ALL)
         return "Offline", None
